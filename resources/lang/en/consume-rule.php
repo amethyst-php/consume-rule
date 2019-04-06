9298341e48fb7e4543021ab5205a8f7768107fd2
railken <railken@protonmail.ch>
2019-04-06 22:40:46 +0200

    add lang

---
+++ resources/lang/en/consume-rule.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'name' => 'Consume Rule',
+    'description' => 'Consume Rule',
+    'attributes' => [
+        'class_name' => [
+            'label' => 'Class',
+            'description' => 'The class used to calculate the consumption'
+        ],
+        'payload' => [
+            'label' => 'Payload',
+            'description' => 'The configuration used by the class to calculate the consumption'
+        ]
+    ]
+]
---
+++ resources/lang/it/consume-rule.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'name' => 'Regola di Consumazione',
+    'description' => 'Regola di Consumazione',
+    'attributes' => [
+        'class_name' => [
+            'label' => 'Classe',
+            'description' => 'La classe utilizzata per il calcolo della consumazione'
+        ],
+        'payload' => [
+            'label' => 'Payload',
+            'description' => 'Configurazione usata dalla classe per il calcolo della consumazione'
+        ]
+    ]
+]
