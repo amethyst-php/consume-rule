cb658df5511eb487dd6f51cb45c0d364052c7ece
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ConsumeRuleManager.php
+++ src/Managers/ConsumeRuleManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\ConsumeRule newEntity()
+ * @method \Amethyst\Schemas\ConsumeRuleSchema getSchema()
+ * @method \Amethyst\Repositories\ConsumeRuleRepository getRepository()
+ * @method \Amethyst\Serializers\ConsumeRuleSerializer getSerializer()
+ * @method \Amethyst\Validators\ConsumeRuleValidator getValidator()
+ * @method \Amethyst\Authorizers\ConsumeRuleAuthorizer getAuthorizer()
+ */
 class ConsumeRuleManager extends Manager
 {
     use ConfigurableManager;
